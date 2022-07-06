a8f0410e7eb96edc0962b3ca28004342e0cb8762
Jero Apriyansah <jeroapriyansah11@gmail.com>
2022-07-06 16:31:06 +0700

    Example Implement permisssion in Blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            @can('admin_access')
+                Admin Access
+            @endcan
+            @can('user_access')
+                User Access
+            @endcan
         </h2>
     </x-slot>
 
