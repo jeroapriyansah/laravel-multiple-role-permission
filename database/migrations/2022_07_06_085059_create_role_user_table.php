3ba349f7667cea273c10fa843b3b5d415aa38fb7
Jero Apriyansah <jeroapriyansah11@gmail.com>
2022-07-06 16:12:42 +0700

    setup migration role_user pivot table

--- database/migrations/2022_07_06_085059_create_role_user_table.php
+++ database/migrations/2022_07_06_085059_create_role_user_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('role_id')->references('id')->on('roles')->cascadeOnDelete();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
